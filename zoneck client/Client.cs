1ed43d6982180dd65383bcc153d67c0472625c94
zonetecde <zonetecde@gmail.com>
2022-09-24 21:30:31 +0200

    Ajoutez des fichiers projet.

---
+++ zoneck client/Client.cs
@@ -0,0 +1,132 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace zoneck_client
+{
+    public class zoneck_client
+    {
+        private Socket SocketClient { get; set; }
+        internal static string ConnetionId { get; set; }
+        public string AppName { get; }
+        internal Action<Message> Receive { get; }
+
+        internal zoneck_client(string appName, string ip, int port, Action<Message> Receive)
+        {
+            //Create an instance
+            SocketClient = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            IPAddress _ip = IPAddress.Parse(ip);
+            IPEndPoint point = new IPEndPoint(_ip, port);
+            //Make connection
+            SocketClient.Connect(point);
+
+            //Receive messages from the server continuously
+            Thread thread = new Thread(_Receive);
+            thread.IsBackground = true;
+            thread.Start();
+
+            // le nom de l'application qui précède les messages [APP] id > message 
+            AppName = "[" + appName + "]";
+
+            this.Receive = Receive;
+        }
+
+        /// <summary>
+        /// Envois un message au serveur
+        /// </summary>
+        /// <param name="str">Le message à envoyer</param>
+        internal void Send(string str)
+        {
+            // [APP NAME] id > message 
+            var buffter = Encoding.UTF8.GetBytes(AppName + " " + ConnetionId + " > " + str + "\r\n");
+            var temp = SocketClient.Send(buffter);
+        }
+
+        private void _Receive()
+        {
+            while (true)
+            {
+                //Get the message sent
+                byte[] buffer = new byte[1024 * 1024 * 2];
+                var effective = SocketClient.Receive(buffer);
+
+
+                var str = Encoding.UTF8.GetString(buffer, 0, effective);
+
+                if (!String.IsNullOrEmpty(str))
+                {
+                    if (str.Contains(AppName) || str.Contains("[server-connexion]")) // [server-connexion] est uniquement envoyé à la personne qui s'est connecté 
+                    {
+                        if (!str.Contains("[server-connexion]"))
+                            str = str.Remove(0, (AppName).Length + 1); // On sait que le message vient de l'application là
+
+                        if (str.Contains(" %last_message%")) // si c'est un last message
+                        {
+                            Receive(
+                            new Message(str.Substring(0, str.IndexOf(" %last_message%", StringComparison.Ordinal)), String.Empty,
+                            AppName
+                            , MESSAGE_TYPE.LAST_MESSAGE)
+                            );
+                        }
+                        else if (str.Contains(" %connection%")) // si c'est une connexion
+                        {
+                            Receive(
+                            new Message(str.Substring(0, str.IndexOf(" %connection%", StringComparison.Ordinal)), String.Empty,
+                            AppName
+                            , MESSAGE_TYPE.CONNECTION)
+                            );
+                        }
+                        else if (str.Contains(" %disconnection%")) // si c'est une deconnexion 
+                        {
+                            Receive(
+                            new Message(str.Substring(0, str.IndexOf(" %disconnection%", StringComparison.Ordinal)), String.Empty, AppName
+        , MESSAGE_TYPE.DISCONNECTION)
+                            );
+                        }
+                        else if (str.Contains(" > ")) // si c'est un message
+                        {
+                            Receive(
+                                new Message(str.Substring(0, str.IndexOf(" > ", StringComparison.Ordinal)), // [id] > message
+                                str.Substring(str.IndexOf(" > ") + " > ".Length) // id > [message]
+                                    .Replace("\r\n", string.Empty), AppName
+        , MESSAGE_TYPE.MESSAGE)
+                                );
+                        }
+                        else // si c'est pour informer de son id de session et informer ensuite le serveur que nous sommes de l'app
+                        {
+                            ConnetionId = str.Substring(str.IndexOf("[server-connexion] as ") + "[server-connexion] as ".Length).Replace("\r\n", string.Empty);
+
+                            var buffter = Encoding.UTF8.GetBytes(AppName + " " + ConnetionId + " %connection%" + "\r\n");
+                            var temp = SocketClient.Send(buffter);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    internal class Message
+    {
+        public Message(string id, string content, string appName, MESSAGE_TYPE messageType)
+        {
+            Id = id;
+            Content = content;
+            MessageType = messageType;
+            AppName = appName;
+        }
+
+        internal string Id { get; set; }
+        internal string Content { get; set; }
+        internal string AppName { get; set; }
+
+        internal MESSAGE_TYPE MessageType { get; set; }
+    }
+
+    internal enum MESSAGE_TYPE
+    {
+        MESSAGE,
+        CONNECTION,
+        DISCONNECTION,
+        LAST_MESSAGE
+    }
+}
---
+++ zoneck server/Server.cs
@@ -0,0 +1,164 @@
+﻿using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase.Protocol;
+
+namespace zoneck_server
+{
+    public class Server
+    {
+        static AppServer zoneck_server { get; set; }
+        static string IP { get; set; } = "127.0.0.1";
+        static int Port { get; set; } = 30_000;
+
+        private static List<User> usersConnected = new List<User>();
+
+        static void Main(string[] args)
+        {
+            zoneck_server = new AppServer();
+
+            var m_Config = new ServerConfig
+            {
+                Port = Port,
+                Mode = SocketMode.Tcp,
+                Name = "serveur",
+                TextEncoding = "UTF-8",
+                Ip = IP,
+            };
+
+
+            //Setup the appServer
+            if (!zoneck_server.Setup(m_Config))//Setup with listening port
+            {
+                Console.WriteLine("Failed to setup!");
+                Console.ReadKey();
+                return;
+            }
+
+            //Try to start the appServer
+            if (!zoneck_server.Start())
+            {
+                Console.WriteLine("Failed to start!");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("The server started successfully, press key'q' to stop it! \nIp : " + zoneck_server.Config.Ip + "\nPort : " + zoneck_server.Config.Port); ;
+
+            //1.
+            zoneck_server.NewSessionConnected += new SessionHandler<AppSession>(appServer_NewSessionConnected);
+            zoneck_server.SessionClosed += appServer_NewSessionClosed;
+
+            //2.
+            zoneck_server.NewRequestReceived += new RequestHandler<AppSession, StringRequestInfo>(appServer_NewRequestReceived);
+
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                Console.WriteLine();
+                continue;
+            }
+
+            //Stop the appServer
+            zoneck_server.Stop();
+
+            Console.WriteLine("The server was stopped!");
+            Console.ReadKey();
+        }
+
+        //1.
+        static void appServer_NewSessionConnected(AppSession session)
+        {
+            session.Send("[server-connexion] as " + session.SessionID);
+
+            Console.WriteLine("[server-connexion] as " + session.SessionID);
+
+            usersConnected.Add(new User(session.SessionID));
+        }
+
+        static void appServer_NewSessionClosed(AppSession session, CloseReason aaa)
+        {
+            string disconnectionMessage = usersConnected.FirstOrDefault(x => x.userID == session.SessionID).userApp + " " + session.SessionID + " %disconnection%";
+
+            foreach (var u in zoneck_server.GetAllSessions())
+            {
+                if (u.SessionID != session.SessionID)
+                    u.Send(disconnectionMessage);
+            }
+
+            Console.WriteLine(DateTime.Now.ToString() + " - " + disconnectionMessage);
+
+            usersConnected.RemoveAll(x => x.userID == session.SessionID);
+
+            Console.WriteLine("utilisateur connecté au serveur " + usersConnected.Count);
+        }
+
+        //2.
+        static void appServer_NewRequestReceived(AppSession session, StringRequestInfo requestInfo)
+        {
+            string message = (requestInfo.Key + " " + requestInfo.Body);
+
+            int pFrom = message.IndexOf("] ") + "] ".Length;
+            int pTo = message.LastIndexOf(" %");
+
+            string userId = message.Substring(pFrom, pTo - pFrom);
+
+            Console.WriteLine(DateTime.Now.ToString() + " - " + message);
+            //session.Send(requestInfo.Key);
+
+            usersConnected.FirstOrDefault(x => x.userID == userId).lastMessage = message;
+
+            if (message.Contains("%connection%"))
+            {
+
+
+                string appName = message.Substring(
+                    0,
+                    message.IndexOf("]") + 1);
+
+                usersConnected.FirstOrDefault(x => x.userID == userId).userApp = appName;
+            }
+            else if(message.Contains("%last_message%")) // send le last message de toutes les personnes de l'app
+            {
+                foreach (var u in zoneck_server.GetAllSessions().ToList())
+                {
+                    if ((u.SessionID != session.SessionID)
+                        &&
+                            usersConnected.FirstOrDefault(x => x.userID == u.SessionID).userApp == message.Substring( // envois uniquement au gens de l'app
+                        0,
+                        message.IndexOf("]") + 1))
+
+                    session.Send(usersConnected.FirstOrDefault(x => x.userID == u.SessionID).lastMessage + " %last_message%");
+                }
+
+                return;
+            }
+
+
+            foreach (var u in zoneck_server.GetAllSessions().ToList())
+            {
+                if ((u.SessionID != session.SessionID)
+                    &&
+                        usersConnected.FirstOrDefault(x => x.userID == u.SessionID).userApp == message.Substring( // envois uniquement au gens de l'app
+                    0,
+                    message.IndexOf("]") + 1))
+
+                    u.Send(message);
+            }
+        }
+
+
+
+    }
+
+    internal class User
+    {
+        internal string userID;
+        internal string userApp;
+        internal string lastMessage;
+
+        public User(string userID)
+        {
+            this.userID = userID;
+            this.userApp = userApp;
+        }
+    }
+}
