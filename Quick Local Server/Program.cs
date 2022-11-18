1a2a1db2dab1f624c3c49c0af3e22ec0bf0d82f4
zonetecde <zonetecde@gmail.com>
2022-11-18 20:13:25 +0100

    Added setup

--- Quick Local Server/Program.cs
+++ Quick Local Server/Program.cs
@@ -11,7 +11,24 @@ namespace Quick_Local_Server
     {
         static void Main(string[] args)
         {
-            ZoneckServer serveur = new ZoneckServer();
+            START:
+            Console.WriteLine("Entrez une adresse Ip ou laisser vide pour utiliser : 127.0.0.1 (fonctionnera sur cette ordinateur)");
+            string output = Console.ReadLine();
+            string ip = String.IsNullOrWhiteSpace(output) ? "127.0.0.1" : output;
+
+            Console.WriteLine("Entrez un port ou laisser vide pour utiliser : 30000");
+            output = Console.ReadLine();
+            string port = String.IsNullOrWhiteSpace(output) ? "30000" : output;
+
+            try
+            {
+                ZoneckServer serveur = new ZoneckServer(ip, Convert.ToInt32(port));
+            }
+            catch
+            {
+                Console.WriteLine("Information invalide.");
+                goto START;
+            }
         }
     }
 }
